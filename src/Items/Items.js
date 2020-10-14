8a7883d28ab5651ec4ac746ea6baaaf48f47bf03
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-10-14 09:10:48 -0500

    added template code

---
+++ src/Items/Items.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Items() {
+  return(
+    <div></div>
+  )
+}
+
+export default Items;
---
+++ src/Warranty/Warranty.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Warranty() {
+  return(
+    <div></div>
+  )
+}
+
+export default Warranty;
